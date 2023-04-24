f7ddd1b26e74dc68da996db5f1ae62b6477bf55d
saranppp <118053089+saranppp@users.noreply.github.com>
2023-04-24 09:48:16 +0530

    Add files via upload

---
+++ app/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+CATEGORY_CHOICES=(
+    ('CR','Curd'),
+    ('ML','Milk'),
+    ('LS','Lassi'),
+    ('MS','Milkshake'),
+    ('GH','Ghee'),
+    ('CZ','Cheese'),
+    ('IC','Ice-ream'),
+)
+
+STATE_CHOICES=(('kerala','kerala'),
+               ('tamil nadu','tamil nadu'))
+
+class Product(models.Model):
+    title=models.CharField(max_length=100)
+    selling_price=models.FloatField()
+    discounted_price=models.FloatField()
+    description=models.TextField()
+    composition=models.TextField(default='')
+    prodapp=models.TextField(default='')
+    category=models.CharField(choices=CATEGORY_CHOICES,max_length=2)
+    product_image=models.ImageField(upload_to='product')
+    def __str__(self):
+        return self.title
+    
+class Customer(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    name=models.CharField(max_length=200)
+    locality=models.CharField(max_length=200)
+    city=models.CharField(max_length=50)
+    mobile=models.IntegerField(default=0)
+    zipcode=models.IntegerField()
+    state=models.CharField(choices=STATE_CHOICES,max_length=100)
+    def __str__(self):
+                return self.name
---
+++ app/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render
+from django.db.models import Count
+from django.http import HttpResponse
+from django.views import View
+from .models import Product,Customer
+from .forms import CustomerRegistrationForm,CustomerProfileForm
+from django.contrib import messages
+import razorpay
+
+# Create your views here.
+def home(request):
+    return render(request,"app/home.html")
+
+def about(request):
+    return render(request,"app/about.html")
+
+def contact(request):
+    return render(request,"app/contact.html")
+
+class Category(View):
+    def get(self,request,val):
+        product=Product.objects.filter(category=val)
+        title=Product.objects.filter(category=val).values('title').annotate(total=Count('title'))
+        return render(request,"app/category.html",locals())
+    
+class CategoryTitle(View):
+    def get(self,request,val):
+        product=Product.objects.filter(title=val)
+        title=Product.objects.filter(category=product[0].category).values('title') 
+        return render(request,"app/category.html",locals())
+
+class ProductDetail(View):
+    def get(self,request,pk):
+        product=Product.objects.get(pk=pk)
+        return render(request,'app/productdetail.html',locals())
+    
+class CustomerRegistrationView(View):
+    def get(self,request):
+        form=CustomerRegistrationForm()
+        return render(request,'app/customerregistration.html',locals())
+    def post(self,request):
+        form=CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"User registration Succesful")
+        else:
+            messages.warning(request,'Invalid Input Data')
+        return render(request,'app/customerregistration.html',locals())
+
+class ProfileView(View):
+    def get(self,request):
+        form=CustomerProfileForm()
+        return render (request,'app/profile.html', locals())
+    def post(self,request):
+        form=CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user=request.user
+            name=form.cleaned_data['name']
+            locality=form.cleaned_data['locality']
+            city=form.cleaned_data['city']
+            mobile=form.cleaned_data['mobile']
+            state=form.cleaned_data['state']
+            zipcode=form.cleaned_data['zipcode']
+            reg=Customer(user=user,name=name,locality=locality,city=city,mobile=mobile,state=state,zipcode=zipcode)
+            reg.save()
+            messages.success(request,"profile saved")
+        else:
+            messages.warning(request,'Invalid data')
+        return render (request,'app/profile.html', locals()) 
+
+def address(request):
+    add=Customer.objects.filter(user=request.user)
+    return render(request,'app/address.html',locals())
